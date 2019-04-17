79c6a4fdc566c659dbcfd2db030d3ce20028ba15
Justin Cooper <jwcooper@gmail.com>
2019-04-17 10:10:20 -0500

    styled buttons for library downloads, update content

--- assets/javascript/libraries.js
+++ assets/javascript/libraries.js
@@ -6,28 +6,35 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   function buildReleaseLinks(data) {
-    console.log(data);
     let releaseList = document.querySelector('.release-list');
 
     data.assets.forEach(function(asset) {
       let name = asset.name;
 
       if (name.slice(-6) === 'ignore') {
+        // any .ignore files in the assets list
         return;
       }
 
       let versionId = name.replace(/(-[\d]+.zip$)/, '');
+      let versionElement = document.getElementById(versionId);
 
-      console.log(name);
-      return;
-      let divElement = document.createElement('div');
+      if (!versionElement) {
+        // likely an older version we don't want to link to, such as 2.x
+        return;
+      }
+
+      let iconElement = document.createElement('i');
+      iconElement.className = "fas fa-download";
+
       let linkElement = document.createElement('a');
-      linkElement.title = asset.name;
+      linkElement.title = "Library Bundle Download";
       linkElement.href = asset.browser_download_url;
+      linkElement.className = "purple-button-link";
       let linkText = document.createTextNode(asset.name);
       linkElement.appendChild(linkText);
-      liElement.appendChild(linkElement);
-      releaseList.appendChild(liElement);
+      linkElement.appendChild(iconElement);
+      versionElement.appendChild(linkElement);
     });
   }
 
