e2e8f435b377fb2b54aa632b02bad7ca4fed69c5
Justin Cooper <jwcooper@gmail.com>
2019-10-14 14:21:18 -0500

    select and show the libraries based on label

---
+++ assets/javascript/contributing.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var issueSelect = document.querySelector(".open-issues select");
+  issueSelect.onchange = issueSelectHandler;
+}, false);
+
+function issueSelectHandler(event) {
+  var selectedOption = this.options[this.selectedIndex].value;
+
+  // hide all elements first
+  [].forEach.call(document.querySelectorAll('.issues-list li'), function (element) {
+    element.style.display = 'none';
+    element.parentElement.closest('li').style.display = 'none';
+  });
+
+  // show the selected options
+  var items = document.querySelectorAll(`.issues-list .${selectedOption}`);
+  items.forEach(function(item) {
+    item.style.display = 'block'
+    item.parentElement.closest('li').style.display = 'block';
+  });
+}
+
