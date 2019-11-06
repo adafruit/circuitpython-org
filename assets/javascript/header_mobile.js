dc1adb75bde2d25616feaab9e921cd1e2f422671
Justin Cooper <jwcooper@gmail.com>
2019-11-06 10:51:54 -0600

    add close icon toggle to mobile menu

--- assets/javascript/header_mobile.js
+++ assets/javascript/header_mobile.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("mobile-menu-button").addEventListener('click', handleMobileToggle);
+  document.getElementById('mobile-menu-button').addEventListener('click', handleMobileToggle);
 });
 
 function handleMobileToggle(event) {
@@ -8,4 +8,11 @@ function handleMobileToggle(event) {
   var menuContainer = document.getElementById('mobile-menu-contents');
 
   menuContainer.classList.toggle('hidden');
+
+  var menuIcon = document.querySelector('#mobile-menu-button > i');
+  if (menuContainer.classList.contains('hidden')) {
+    menuIcon.classList.replace('fa-times', 'fa-bars');
+  } else {
+    menuIcon.classList.replace('fa-bars', 'fa-times');
+  }
 }
