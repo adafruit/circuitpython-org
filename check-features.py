89effd7dce7696020301ec871424fdda575079e6
makermelissa <makermelissa@users.noreply.github.com>
2022-05-10 21:13:31 +0000

    deploy: 7957a441f03ef1a1e5839290522a783fa03e497e

--- check-features.py
+++ check-features.py
@@ -2,17 +2,42 @@
 from pathlib import Path
 import frontmatter
 
+# Check CircuitPython Download Features
 with open('template.md', "rt") as f:
     metadata, content = frontmatter.parse(f.read())
     acceptable_features = set(metadata['features'])
 
-failed = False
-for filename in Path('_board').glob("*.md"):
-    with open(filename, "rt") as f:
-        metadata, content = frontmatter.parse(f.read())
-    features = metadata.get('features') or ()
-    for feature in sorted(set(features) - acceptable_features):
-        print(f"{filename}:0: Non-standard feature: {feature}")
-        failed = True
+def verify_features(folder, valid_features):
+    success = True
+    for filename in Path(folder).glob("*.md"):
+        with open(filename, "rt") as f:
+            metadata, content = frontmatter.parse(f.read())
+        features = metadata.get('features') or ()
+        for feature in sorted(set(features) - valid_features):
+            print(f"{filename}:0: Non-standard feature: {feature}")
+            success = False
+    return success
+
+if not verify_features("_board", acceptable_features):
+    print("Non-standard features found. See https://learn.adafruit.com/how-to-add-a-new-board-to-the-circuitpython-org-website/adding-to-downloads for acceptable features")
+    raise SystemExit(True)
+
+# Check Blinka Download Features
+blinka_features = {
+    "Ethernet",
+    "HDMI",
+    "Wi-Fi",
+    "40-pin GPIO",
+    "GPS",
+    "Feather-Compatible",
+    "Bluetooth/BLE",
+    "STEMMA QT/QWIIC",
+    "USB 3.0",
+    "Infrared Receiver",
+}
+
+failed = not verify_features("_blinka", blinka_features)
+if failed:
+    print("Non-standard features found. See https://learn.adafruit.com/how-to-add-a-new-board-to-the-circuitpython-org-website/adding-to-blinka for acceptable features")
 
 raise SystemExit(failed)
